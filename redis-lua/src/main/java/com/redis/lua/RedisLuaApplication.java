fd45dd113b969be5d471c3b2f44649ed08d79b18
lizhifu <lizhifuabc@163.com>
2021-08-13 11:45:05 +0800

    init

---
+++ redis-lua/src/main/java/com/redis/lua/RedisLuaApplication.java
@@ -0,0 +1,17 @@
+package com.redis.lua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * redis lua
+ *
+ * @author lizhifu
+ * @date 2021/8/13
+ */
+@SpringBootApplication
+public class RedisLuaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLuaApplication.class, args);
+    }
+}
